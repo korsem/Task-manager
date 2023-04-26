284efef2b63cc0f1b1eda18f0d9ff6648a0c0d25
korsem <korneliasem@gmail.com>
2023-04-26 10:34:33 +0200

    + api detail view

--- tasks_api/urls.py
+++ tasks_api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 # from django.conf.urls import url
 from .views import (
-TaskListApiView,
+    TaskListApiView,
+    TaskDetailApiView
 )
 
 urlpatterns = [
     path('api', TaskListApiView.as_view()),
+    path('api/<int:task_id>/', TaskDetailApiView.as_view())
 ]
--- tasks_api/views.py
+++ tasks_api/views.py
@@ -27,3 +27,50 @@ class TaskListApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class TaskDetailApiView(APIView):
+    # adding permission to check if user is authnticated
+    permission_classes = [permissions.IsAuthenticated]
+    # retrieve
+    def get_object(self, request, task_id, *args, **kwargs):
+        task_instance = self.get_object(task_id, request.user.id)
+        if not task_instance:
+            return Response(
+                {"rest": "Object with task id does not exist"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = TaskSerializer(task_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # update
+    def put(self, request, task_id, *args, **kwargs):
+        # update task with given task id if exist
+        task_instance = self.get_object(task_id, request.user.id)
+        if not task_instance:
+            return Response(
+                {"res": "Object with task id does not exist"}
+            )
+        data = {  # check i dont need the other fields
+            'name': request.data.get('name'),
+            'assigned_to': request.user.id,
+        }
+        serializer = TaskSerializer(instance=task_instance, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # delete
+    def delete(self, request, task_id, *args, **kwargs):
+        # deletes the given task with task_id if exists
+        task_instance = self.get.object(task_id, request.user.id)
+        if not task_instance:
+            return Response(
+                {"res": "Object with task id does not exist"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        task_instance.delete()
+        return Response(
+            {"res": "Object deleted!"},
+            status=status.HTTP_200_OK
+        )
+
